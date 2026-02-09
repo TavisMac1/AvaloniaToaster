ec1dea8f70121b5501e52d48217885b7cefc024d
tavis <macfarlanetavis@protonmail.com>
2026-02-08 21:09:12 -0400

    add default warning theme

---
+++ Themes/AvaloniaToasterWarningTheme.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Layout;
+using Avalonia.Media;
+using AvaloniaToaster.Interfaces;
+
+namespace AvaloniaToasterDevProject.Themes;
+
+public class AvaloniaToasterWarningTheme : IAvaloniaToasterThemes
+{
+    public IBrush BackgroundColor => Avalonia.Media.Brushes.Yellow;
+
+    public IBrush ForegroundColor => Avalonia.Media.Brushes.Black;
+
+    public HorizontalAlignment? HorizontalAlignment => null;
+
+    public VerticalAlignment? VerticalAlignment => null;
+
+    public double? BorderRadius => 3;
+}
